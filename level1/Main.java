3b518c739f6e1b6101bd583a10077a0af03cd7c2
chen-Leo <37828154+chen-Leo@users.noreply.github.com>
2018-11-02 17:57:41 +0800

    Update Main.java

--- level1/Main.java
+++ level1/Main.java
@@ -1,28 +1,86 @@
 package test2;
 
-import test2.Animal;
-import test2.Bird;
-import test2.Dog;
-import test2.Fish;
-
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) {
-	     Animal one = new Animal("肉","雄性", "大地",5,50);
-	     Bird two = new Bird("肉","雄性", "天空" ,5,50);
-	     Dog three = new Dog("杂食","雄性", "大地" ,5,50);
-	     Fish four = new Fish("植物","无性别", "海洋" ,7,45);
-	     System.out.println("animal:");
-	     one.showDetails();
-	     one.move();
-	     System.out.println("bird:");
-	     two.showDetails();
-	     two.move();
-	     System.out.println("dog:");
-	     three.showDetails();
-	     three.move();
-	     System.out.println("fish:");
-	     four.showDetails();
-	     four.move();
+	// 判断数字
+	public static boolean isNum(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			int chr = str.charAt(i);
+			if (chr < 48 || chr > 57)
+				return false;
+		}
+		return true;
+	}
+
+	// 判断大写
+	public static boolean isUpChar(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			int chr = str.charAt(i);
+			if (!((int) str.charAt(i) > 64 && (int) str.charAt(i) < 91))
+				return false;
+		}
+		return true;
+	}
+
+	// 判断小写
+	public static boolean isLowChar(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			int chr = str.charAt(i);
+			if (!((int) str.charAt(i) > 96 && (int) str.charAt(i) < 123))
+				return false;
+		}
+		return true;
+	}
+
+	// 判断是否全为字母
+	public static boolean isChar(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			int chr = str.charAt(i);
+			if (!((int) str.charAt(i) > 96 && (int) str.charAt(i) < 123)
+					|| ((int) str.charAt(i) > 64 && (int) str.charAt(i) < 91))
+				return false;
+		}
+		return true;
+	}
+
+	// 小写转大写
+	public static String lowToUp(String str) {
+		if (!isChar(str)) {
+			System.out.println("error ,not all chars");
+			System.exit(-1);
+		}
+		str = str.toUpperCase();
+		return str;
+	}
+
+	// 数字顺序输出
+	public static void printMath(String str) {
+
+		if (!isNum(str)) {
+			System.out.println("error ,not all mathnum");
+			System.exit(-1);
+		}
+
+		char[] a = str.toCharArray();
+		Arrays.sort(a);
+		for (int i = 0; i < a.length; i++)
+			System.out.print(a[i]);
+
+	}
+
+	// 字母顺序输出
+	public static void printChar(String str) {
+
+		if (!isChar(str)) {
+			System.out.println("error ,not all char");
+			System.exit(-1);
+		}
+
+		char[] a = str.toCharArray();
+		Arrays.sort(a);
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]);
+		}
 	}
 }
